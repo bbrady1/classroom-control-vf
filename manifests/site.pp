52bbd79d23f09b677651abba0708f4b43200afc5
bbrady1 <handb.brady@frontier.com>
2016-05-24 08:38:53 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,7 +46,7 @@ node default {
   
   file {'/etc/motd':
     ensure      => file,
-    owerner     => 'root',
+    owner       => 'root',
     group       => 'root',
     mode        => '640',
     content     => 'Puppet is confusing..',
