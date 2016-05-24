2399f03c0b8ef33e5cbabe63758beaa48bbc9007
bbrady1 <handb.brady@frontier.com>
2016-05-24 14:49:59 -0500

    recreated memcached init.pp
    created the manifest directory and recreated the init.pp for memcached.

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,18 @@
+class memcached {
+  package {'memcached':
+    ensure => present,
+    }
+  file {'memcached config':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/memcached/memcached.conf',
+    require => Package['memcached'],
+    }
+  servie {'memcached':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['memcached config'],
+    }
+  }
