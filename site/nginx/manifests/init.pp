df0925dcb5325265505b126c3c8f58e8a78dc280
bbrady1 <handb.brady@frontier.com>
2016-05-25 09:21:43 -0500

    started over
    recreated init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,54 +1,14 @@
-class ngnix {
-  package {'ngnix':
-    ensure => present,
+class nginx {
+  file {'nginx rpm':
+    ensure  => file,
+    path    => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    source  => 'puppet:///modules/ngnix/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
   }
   
-  file {'/var/www':
-    ensure => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+  package { 'nginx': 
+    ensure => '1.6.2-1.el7.centos.ngx',
+    source  => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    provider => rpm,
+    require => File['nginx rpm'],
   }
-  
-  file {'ngnix dir':
-    ensure => directory,
-    path    => '/etc/nginx',
-  }
-  
-  file {'ngnix conf':
-    ensure => present,
-    path    => '/etc/nginx/nginx.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/ngnix/ngnix.conf',
-    require = File['nginx dir'],
-  }
-  
-  file {'/etc/ngnix/conf.d':
-    ensure => directory,
-  }
-  
-  file {'ngnix default':
-    ensure => present,
-    path    => '/etc/ngnix/conf.d/default.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/ngnix/default.conf',
-    require => File['/etc/ngnix/conf.d'],
-  }
-  
-  file {'index html':
-    ensure => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/ngnix/index.html',
-    require => File['/var/www'],
-  }
-  
-  service {'ngnix':
-    ensure  => running,
-  }
-}  
+}
