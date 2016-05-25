85ef8a7bf3ddb401cc82a5ad11c4aca3c57e856b
bbrady1 <handb.brady@frontier.com>
2016-05-25 10:35:42 -0500

    removed duplicates
    removed the duplicate file "nginx" and package.

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -51,19 +51,6 @@ class nginx {
     before  => Package['nginx'],
   }
   
-  file {'nginx rpm':
-    ensure  => file,
-    path    => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
-    source  => 'puppet:///modules/ngnix/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
-  }
-  
-  package { 'nginx': 
-    ensure => '1.6.2-1.el7.centos.ngx',
-    source  => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
-    provider => rpm,
-    require => File['nginx rpm'],
-  }
-  
   file { 'nginx rpm' : 
      ensure   => file, 
      path     => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm', 
